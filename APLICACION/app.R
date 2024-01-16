d33c6c9e4b4dba0bb80f3e9b10773846634c36f3
ANGEL A. URBINA <angel.urbina.sanchez@gmail.com>
2024-01-16 19:25:21 +0100

    Add files via upload

--- APLICACION/app.R
+++ APLICACION/app.R
@@ -29,6 +29,12 @@ if (!require("rsconnect", quietly = TRUE)) {
     library(rsconnect)
 }
 
+# Instalar tidyr si no está instalado (para trabajar con datos en formato "largo")
+if (!require("shinydashboard", quietly = TRUE)) {
+  install.packages("shinydashboard")
+  library(shinydashboard)
+}
+
 
 library(shiny)
 library(ggplot2)
@@ -60,25 +66,101 @@ data8 <- load_and_transform_data("Taula3_Evolucion_Personas_atendidas2017-2022.x
 data9 <- load_and_transform_data("Taula4_Taxa1000habitantes_jovenes.xlsx")
 data10 <- load_and_transform_data("Taula2_Media_Visitas_2022.xlsx")
 
+# Lista completa de textos para cada conjunto de datos
+datasetTexts <- list(
+  "Ingresos Transtorno de Conducta" = "¿Cuantas personas ingresan por transtorno de conducta?",
+  "Ingresos Urgencias Jóvenes" = "¿Cuantos Ingresos Urgencias Jóvenes?",
+  "Reingresos por Sexo y Edad" = "¿Cuantos Reingresos por Sexo y Edad",
+  "Datos Anorexia" = "Datos Anorexia",
+  "Reingresos Adultos 30 dias" = "¿Reingresos Adultos 30 días?",
+  "Evolucion Personas atendidas 2017-2022" = "Evolución de Personas Atendidas 2017-2022",
+  "Tasa 1000 habitantes jovenes" = "¿Cual es el nivel del problema?",
+  "Media Visitas 2022" = "¿Cual es la Media de Visitas por persona segun tipologia?"
+)
+
+# Lista completa de textos para cada conjunto de datos
+datasetTexts1 <- list(
+  "Ingresos Transtorno de Conducta" = "Los ingresos por transtorno de conducta de adolescentes (hombres) duplican el de las mujeres de la misma edad",
+  "Ingresos Urgencias Jóvenes" = "El 62,3% de los ingresos psiquiatricos de menores van a ser urgentes. Por grupos de edad los que mas han aumentado son los de niños y niñas menores de once años",
+  "Reingresos por Sexo y Edad" = "Se observa que las mujeres reingresan más que los hombres en todos los grupos de edad. Especialmente entre los 18 y 24 años",
+  "Datos Anorexia" = "Los ingresos hospitalarios por transtornos de conducta alimentaria han aumentado progresivamente desde el 2018",
+  "Reingresos Adultos 30 dias" = "Por sexo y edad se observa que las mujeres reingresan mas que los hombres, en todos los rangos de edad pero especialmente entre 18 y 24 años",
+  "Evolucion Personas atendidas 2017-2022" = "La media de visitas por personas atendidas en el 2022 es de 7,4 y ha ido aumentando desde el 2017",
+  "Tasa 1000 habitantes jovenes" = "A partir del 2020 se incrementa la tasa de niños y jovenes atendidos en los centros de salud mental",
+  "Media Visitas 2022" = "En el año 2022 el 39,1% de las personas atendidas son pacientes cronicos de salud mental"
+)
+
 # Interfaz de usuario
 ui <- fluidPage(
+  
   titlePanel("Datos de Salud Mental (Cataluña)"),
+    tags$head(
+      tags$style(HTML("
+      .selected-dataset-text {
+        font-size: 18px; /* Tamaño del texto */
+        color: #2E86C1; /* Color del texto, en formato hexadecimal */
+        font-family: 'Arial', sans-serif; /* Tipo de letra */
+        margin-bottom: 20px; /* Espacio debajo del texto */
+        padding: 10px; /* Relleno alrededor del texto */
+        text-align: center; /* Alineación del texto */
+        background-color: #EAF2F8; /* Color de fondo */
+        border-radius: 5px; /* Bordes redondeados */
+        box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.1); /* Sombra sutil */
+      }
+    "))
+    ),
   sidebarLayout(
     sidebarPanel(
-      selectInput("dataset", "Seleccionar conjunto de datos", choices = c("Ingresos Anorexia", "Ingresos Urgencias Jóvenes", "Reingresos por Sexo y Edad", "Datos Anorexia", "Reingresos Adultos 30 dias", "Evolucion Personas atendidas 2017-2022",  "Tasa 1000 habitantes jovenes", "Media Visitas 2022")),
-      uiOutput("yearInput") # Selector de año
+      selectInput("dataset", "Seleccionar conjunto de datos", choices = c("Ingresos Transtorno de Conducta", "Ingresos Urgencias Jóvenes", "Reingresos por Sexo y Edad", "Datos Anorexia", "Reingresos Adultos 30 dias", "Evolucion Personas atendidas 2017-2022", "Tasa 1000 habitantes jovenes", "Media Visitas 2022")),
+      uiOutput("yearInput"), 
+      actionButton("showModal", "Mostrar Ventana"),
+      # Recuadro mejorado para el texto del dataset1
+      wellPanel(
+        tags$head(
+          tags$style(HTML("
+            .well {
+              background-color: #F7F7F7; /* Color de fondo */
+              border: 1px solid #E3E3E3; /* Borde del recuadro */
+              border-radius: 10px; /* Bordes redondeados */
+              box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.1); /* Sombra sutil */
+              padding: 15px; /* Espaciado interno */
+              margin-top: 20px; /* Espacio superior */
+              margin-bottom: 20px; /* Espacio inferior */
+              font-family: 'Arial', sans-serif; /* Tipo de fuente */
+            }
+            .well h4 {
+              color: #333333; /* Color del título */
+              margin-top: 0; /* Espacio superior del título */
+            }
+          "))
+        ),
+        h4("Información Adicional"),  # Título del recuadro
+        textOutput("selectedDataset1Text")
+      )
     ),
     mainPanel(
+      # Envolver textOutput en un div para aplicar estilos CSS
+      div(textOutput("selectedDatasetText"), class = "selected-dataset-text"),
       plotOutput("linePlot"),
-      textOutput("colorExplanation"), # Texto explicativo para la leyenda de colores
-      dataTableOutput("yearTable")  # Elemento para la tabla
+      textOutput("colorExplanation"),
+      dataTableOutput("yearTable")
     )
   ),
+
+
   # Añadir un pie de página con la fuente de los datos
   tags$footer(
-    tags$p("Fuente: Datos Central de Resultados (Departament Salut Generalitat)", 
+    tags$p("Fuente: Central de Resultats (Salut, Agéncia de Qualitat i Avaluació Sanitàrias de Catalunya)", 
            style = "text-align: center; color: grey; font-size: 0.8em; padding-top: 10px;")
   )
+  ## Ventana emergente
+  #modalDialog(
+  #  title = "Mensaje de Prueba",
+  #  "Hola Prueba",
+  #  easyClose = TRUE,
+  #  footer = NULL,
+  #  id = "testModal" # Identificador de la ventana emergente
+  #)
 )
 
 # Funciones del servidor
@@ -86,7 +168,7 @@ server <- function(input, output) {
   # Actualizar dinámicamente la selección del año
   output$yearInput <- renderUI({
     dataset <- switch(input$dataset,
-                      "Ingresos Anorexia" = data1,
+                      "Ingresos Transtorno de Conducta" = data1,
                       "Ingresos Urgencias Jóvenes" = data2,
                       "Reingresos por Sexo y Edad" = data3,
                       "Ingresos Urgencias Jovenes Por edad" = data4,
@@ -99,9 +181,29 @@ server <- function(input, output) {
     selectInput("year", "Año", choices = unique(dataset$Year))
   })
 
+  # Actualizar el texto basado en la selección del conjunto de datos
+  output$selectedDatasetText <- renderText({
+    selectedDataset <- input$dataset
+    if (selectedDataset %in% names(datasetTexts)) {
+      return(datasetTexts[[selectedDataset]])
+    } else {
+      return("Texto por defecto para conjuntos de datos no especificados")
+    }
+  })
+  
+  # Función para mostrar el texto del dataset1
+  output$selectedDataset1Text <- renderText({
+    selectedDataset <- input$dataset
+    if (selectedDataset %in% names(datasetTexts1)) {
+      return(datasetTexts1[[selectedDataset]])
+    } else {
+      return("Texto por defecto para conjuntos de datos no especificados en dataset1")
+    }
+  })
+  
 output$linePlot <- renderPlot({
   data_selected <- switch(input$dataset,
-                          "Ingresos Anorexia" = data1,
+                          "Ingresos Transtorno de Conducta" = data1,
                           "Ingresos Urgencias Jóvenes" = data2,
                           "Reingresos por Sexo y Edad" = data3,
                           "Ingresos Urgencias Jovenes Por edad" = data4,
@@ -137,7 +239,7 @@ output$linePlot <- renderPlot({
 # Mostrar datos del año seleccionado
   output$yearData <- renderText({
     data_selected <- switch(input$dataset,
-                            "Ingresos Anorexia" = data1,
+                            "Ingresos Transtorno de Conducta" = data1,
                             "Ingresos Urgencias Jóvenes" = data2,
                             "Reingresos por Sexo y Edad" = data3,
                             "Ingresos Urgencias Jovenes Por edad" = data4,
@@ -160,7 +262,7 @@ output$linePlot <- renderPlot({
 # Mostrar tabla de datos del año seleccionado
   output$yearTable <- renderDataTable({  
     data_selected <- switch(input$dataset,
-                            "Ingresos Anorexia" = data1,
+                            "Ingresos Transtorno de Conducta" = data1,
                             "Ingresos Urgencias Jóvenes" = data2,
                             "Reingresos por Sexo y Edad" = data3,
                             "Ingresos Urgencias Jovenes Por edad" = data4,
@@ -179,6 +281,20 @@ if (nrow(selected_year_data) == 0) {
 }
 }, options = list(pageLength = 5)) # Configuración opcional para limitar el número de filas mostradas
 }
+
+
+## Observar el evento del botón showModal
+#observeEvent(input$showModal, {
+#  # Mostrar la ventana emergente
+#  showModal(modalDialog(
+#    title = "PRA01 UCO Visualizacion",
+#    "Hola Prueba",
+#    easyClose = TRUE,  # Permite cerrar la ventana haciendo clic fuera de ella
+#    footer = modalButton("Cerrar")  # Botón para cerrar explícitamente la ventana
+# ))
+#}
+#)
+
 # Ejecutar la aplicación
 shinyApp(ui, server)
 
